06d81e758123e84e3cb468e79d46760f4c869532
strenuousnerd8 <zakkiwat@gmail.com>
2022-03-06 23:49:26 +0530

    Solved 3 more problems before taking a break from coding to prepare for college exams

---
+++ HackerRank/SparseArrays.py
@@ -0,0 +1,39 @@
+
+# There is a collection of input strings and a collection of query strings. For each query string,
+# determine how many times it occurs in the list of input strings. Return an array of the results.
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. STRING_ARRAY strings
+#  2. STRING_ARRAY queries
+#
+
+def matchingStrings(strings, queries):
+    # Write your code here
+    res = []
+    for i in range(len(queries)):
+        res.append(strings.count(queries[i]))
+    return res
+
+if __name__ == '__main__':
+
+    strings_count = int(input().strip())
+
+    strings = []
+
+    for _ in range(strings_count):
+        strings_item = input()
+        strings.append(strings_item)
+
+    queries_count = int(input().strip())
+
+    queries = []
+
+    for _ in range(queries_count):
+        queries_item = input()
+        queries.append(queries_item)
+
+    res = matchingStrings(strings, queries)
+
+    print(res)
