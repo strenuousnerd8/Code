cdf5619db5eac9bab2e24e0abed3e57b56fc1be5
strenuousnerd8 <zakkiwat@gmail.com>
2022-03-08 14:30:05 +0530

    Wrote readable and optimal code for caesar cipher and updated lonely integer program

---
+++ CaesarCipherOpti.py
@@ -0,0 +1,14 @@
+# Optimized CaesarCipher code using formula
+# Encryption E(p) = (p + k) mod 26
+# Decryption E(c) = (c - k) mod 26
+import string
+library, res = string.ascii_lowercase, []
+userText, k, choice = str(input("Enter plain/cipher text :\t")), int(input("Enter Key :\t")), [True, False]
+choice = choice[0] if input("Press E for encryption or D for decryption :\t").lower() == 'e' else choice[1]
+if choice:
+    for letter in userText:
+        res.append(library[(library.index(letter) + k) % 26])
+else:
+    for letter in userText:
+        res.append(library[(library.index(letter) - k) % 26])
+print(''.join(res))
