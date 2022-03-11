01001c7f83e5ac0aa03ec8421379a1e4f19bc719
strenuousnerd8 <zakkiwat@gmail.com>
2022-03-11 23:41:58 +0530

    Optimal code for Permuting Arrays

---
+++ HackerRank/PermutingTwoArrays.py
@@ -0,0 +1,38 @@
+#
+# Complete the 'twoArrays' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY A
+#  3. INTEGER_ARRAY B
+#
+
+def twoArrays(k, A, B):
+    # Write your code here
+    A = sorted(A)
+    B = sorted(B, reverse=True)
+    flag = 0
+    for i in range(len(A)):
+        if A[i] + B[i] >= k:
+            flag += 1
+    return 'YES' if flag == len(A) else 'NO'
+
+if __name__ == '__main__':
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        k = int(first_multiple_input[1])
+
+        A = list(map(int, input().rstrip().split()))
+
+        B = list(map(int, input().rstrip().split()))
+
+        result = twoArrays(k, A, B)
+
+        print(result + '\n')
