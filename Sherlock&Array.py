f63273bcf568ff0563665934889c225158e80b0f
strenuousnerd8 <zakkiwat@gmail.com>
2022-03-15 23:20:18 +0530

    Solved !

---
+++ Sherlock&Array.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'balancedSums' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+# 1
+# 4
+# 1 2 3 4
+
+def balancedSums(arr):
+    # Write your code here
+    # Solution 1 (Time inefficient)
+    # return 'YES' if [True for i in range(len(arr)) if sum(arr[:i]) == sum(arr[i+1:])] else 'NO'
+    # Solution 2 (Time Efficient)
+    rp = sum(arr)
+    lp = 0
+    for i in arr:
+        rp-=i
+        if lp == rp:
+            return 'YES'
+        lp+=i
+    return 'NO'
+
+if __name__ == '__main__':
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = balancedSums(arr)
+
+        print(result)
