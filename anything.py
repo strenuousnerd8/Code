eab449ff19f1530822c41ab3bcd82a3c7477a8f0
strenuousnerd8 <zakkiwat@gmail.com>
2022-03-16 23:18:02 +0530

    removed !

--- anything.py
+++
@@ -1,7 +0,0 @@
-def group_list(group, users):
-    members = ','.join(users)
-    return "{group}: {members}".format(group = group, members = members)
-
-print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
-print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
-print(group_list("Users", "")) # Should be "Users:"
