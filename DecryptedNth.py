736ef06635ba98f78522231c96f6b2af1bf67adf
strenuousnerd8 <zakkiwat@gmail.com>
2022-03-18 18:11:59 +0530

    O(n) efficiency

--- DecryptedNth.py
+++ DecryptedNth.py
@@ -2,9 +2,7 @@
 class UserMainCode(object):
     @classmethod
     def characterAt(cls, input1, input2):
-        res = []
-        for i in range(0, len(input1), 2):
-            res.append(input1[i] * int(input1[i+1]))
+        res = ''.join([input1[i] * int(input1[i+1]) for i in range(0, len(input1), 2)])
         try:
             return res[input2-1]
         except IndexError:
