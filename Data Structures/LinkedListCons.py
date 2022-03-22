4048418e4c04920bacb31b95562fecd788d842bd
strenuousnerd8 <zakkiwat@gmail.com>
2022-03-22 18:51:09 +0530

    created pop and append methods for my linked list

--- Data Structures/LinkedListCons.py
+++ Data Structures/LinkedListCons.py
@@ -1,3 +1,6 @@
+from jinja2 import Undefined
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -10,5 +13,40 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-new = LinkedList(4)
-print(new.head.value)
+    def printlist(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        pre = self.head
+        temp = self.head
+        while temp.next:
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp.value
+
+new = LinkedList(1)
+new.append(2)
+print(new.pop())
+print(new.pop())
+print(new.pop())
