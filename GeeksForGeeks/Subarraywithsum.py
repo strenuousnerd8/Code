d36e176cb259160e2f3ac4e457c51cadce93f824
strenuousnerd8 <zakkiwat@gmail.com>
2022-04-13 13:05:05 +0530

    Improved the output capacity

--- GeeksForGeeks/Subarraywithsum.py
+++ GeeksForGeeks/Subarraywithsum.py
@@ -8,9 +8,10 @@ class Sum(object):
                 if lis[i:j+1] != []:
                     if sum(lis[i:j+1]) == tar:
                         res.append(lis[i:j+1])
-        return lis.index(res[0][0]), lis.index(res[0][1])
+        for i in range(len(res[0])):
+            print(lis.index(res[0][i]))
 
-print(Sum.targetSum(
+Sum.targetSum(
     list(map(int, input().split())),
     int(input())
-))
+)
