e8fbabc356bb7d49d259304bf31702797f914feb
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-11 15:10:12 +0530

    Yet to be improved further

--- HackerRank/NewYearChaos.py
+++ HackerRank/NewYearChaos.py
@@ -1,11 +1,19 @@
-def minimumBribes(n, q):
+def minimumBribes(q):
     # Write your code here
-    comp = list(range(1, n+1))
-    print(comp, q, sep="\n")
-    changed = []
-    for i in range(n):
-        if comp[i] != q[i]:
-            changed.append(q[i])
+    result=0
+    for x in range(len(q)):
+        if q[x] - x-1 >2:
+            print("Too chaotic")
+            return
+        else:
+            ck =x
+            while (ck != 0 and q[ck] < q[ck-1]) or (ck > 1 and q[ck] < q[ck-2]):
+                temp = q[ck]
+                q[ck] =q[ck-1]
+                q[ck-1]=temp
+                ck-=1
+                result+=1
+    print(result)
 
 if __name__ == '__main__':
     t = int(input().strip())
