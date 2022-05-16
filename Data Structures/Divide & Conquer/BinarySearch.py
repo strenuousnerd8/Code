abdf2725715df866b6a121006715978403b88f72
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-16 19:57:05 +0530

    Added ternary hint

--- Data Structures/Divide & Conquer/BinarySearch.py
+++ Data Structures/Divide & Conquer/BinarySearch.py
@@ -9,9 +9,10 @@ def BinarySearch(arr, n, low, high, data):
             return BinarySearch(arr, n, mid + 1, high, data)
         else:
             return BinarySearch(arr, n, low, mid - 1, data)
+        # (or just) return mid if arr[mid] == data else BinarySearch(arr, n, mid + 1, high, data) if arr[mid] < data else BinarySearch(arr, n, low, mid - 1, data)
 
 
-arr = [5, 9, 17, 23, 25, 45, 59, 63, 71, 89]
-n = 10
-data = 59
+arr = [20, 21, 39, 43, 49, 54, 73, 85, 96]
+n = 9
+data = 96
 print(BinarySearch(arr, n, 0, n-1, data))
