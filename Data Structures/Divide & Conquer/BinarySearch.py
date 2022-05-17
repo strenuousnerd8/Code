614beca4acc64dfe8fa63c47c5e7f4939e8aad74
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-17 20:11:06 +0530

    Optimal Solutions!

--- Data Structures/Divide & Conquer/BinarySearch.py
+++ Data Structures/Divide & Conquer/BinarySearch.py
@@ -1,4 +1,4 @@
-def BinarySearch(arr, n, low, high, data):
+def BinarySearch(arr, low, high, data):
     if low > high:
         return -1
     else:
@@ -6,13 +6,13 @@ def BinarySearch(arr, n, low, high, data):
         if arr[mid] == data:
             return mid
         elif arr[mid] < data:
-            return BinarySearch(arr, n, mid + 1, high, data)
+            return BinarySearch(arr, mid + 1, high, data)
         else:
-            return BinarySearch(arr, n, low, mid - 1, data)
+            return BinarySearch(arr, low, mid - 1, data)
         # (or just) return mid if arr[mid] == data else BinarySearch(arr, n, mid + 1, high, data) if arr[mid] < data else BinarySearch(arr, n, low, mid - 1, data)
 
 
 arr = [20, 21, 39, 43, 49, 54, 73, 85, 96]
 n = 9
-data = 96
-print(BinarySearch(arr, n, 0, n-1, data))
+data = 73
+print(BinarySearch(arr, 0, n - 1, data))
