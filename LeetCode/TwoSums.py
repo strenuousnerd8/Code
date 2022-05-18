7ecf4199b6e928eea5213a337f2900a24075498d
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-18 16:12:03 +0530

    Optimally solved using hashmap

--- LeetCode/TwoSums.py
+++ LeetCode/TwoSums.py
@@ -1,10 +1,13 @@
 # Twosum :) ifkyk
 class Solution:
     def twoSum(self, nums, target):
+        complementMap = dict()
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i != j and nums[i] + nums[j] == target:
-                    return [i,j]
+            complement = target - nums[i]
+            if nums[i] in complementMap:
+                return [complementMap[nums[i]], i]
+            else:
+                complementMap[complement] = i
 
 Answer = Solution()
 print(Answer.twoSum([2,7,11,15], 26))
