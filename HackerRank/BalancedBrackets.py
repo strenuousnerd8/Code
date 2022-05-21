8ff8b959c43ce3d147be041ed05d5d52128fe639
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-22 00:02:21 +0530

    Solved!

---
+++ HackerRank/BalancedBrackets.py
@@ -0,0 +1,10 @@
+def isBalanced(s):
+    # Write your code here
+    while '()' in s or '[]' in s or '{}' in s:
+        s = s.replace('()', '').replace('[]', '').replace('{}', '')
+    if len(s) > 0: return "NO"
+    return "YES"
+
+if __name__ == '__main__':
+        result = isBalanced("{{)[](}}")
+        print(result)
