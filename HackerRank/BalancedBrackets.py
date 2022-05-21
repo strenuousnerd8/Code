121999b52df4537ccde207f2985c432f680123b6
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-22 00:05:36 +0530

    updated

--- HackerRank/BalancedBrackets.py
+++ HackerRank/BalancedBrackets.py
@@ -2,8 +2,7 @@ def isBalanced(s):
     # Write your code here
     while '()' in s or '[]' in s or '{}' in s:
         s = s.replace('()', '').replace('[]', '').replace('{}', '')
-    if len(s) > 0: return "NO"
-    return "YES"
+    return "NO" if len(s) > 0 else "YES"
 
 if __name__ == '__main__':
         result = isBalanced(input())
