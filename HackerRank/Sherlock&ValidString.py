5df0e0f7217f397fdc87c57a1e3b087d0770332e
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-21 23:15:40 +0530

    solved!

---
+++ HackerRank/Sherlock&ValidString.py
@@ -0,0 +1,24 @@
+#
+# Complete the 'isValid' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def isValid(s):
+    # Write your code here
+    f = []
+    while len(s) > 0:
+        f.append(s.count(s[0]))
+        s = s.replace(s[0],'')
+    n = min(f)
+    x = max(f)
+    if x == n or (f.count(n) == len(f)-1 and x-n == 1) or (f.count(x) == len(f)-1 and n == 1):
+        return 'YES'
+    return 'NO'
+
+if __name__ == '__main__':
+
+    result = isValid("abcdefghhgfedecba")
+
+    print(result)
