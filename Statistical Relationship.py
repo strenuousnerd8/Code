0863941be5396dae36a729188ce7c8fddbe1a0ed
strenuousnerd8 <zakkiwat@gmail.com>
2022-05-27 22:16:05 +0530

    Removed a program

--- Statistical Relationship.py
+++
@@ -1,9 +0,0 @@
-def findDividedString(str):
-    if(len(str) % 2 == 0):
-        print(str[0:len(str)-1:2])
-    elif(len(str) % 2 != 0):
-        print(str[0:len(str)-1:3])
-
-findDividedString("heyheyhey")
-findDividedString("heyhi")
-findDividedString("hey")
