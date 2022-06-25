9653b6c98a6909ae0d97405302c416bac24b45aa
strenuousnerd8 <zakkiwat@gmail.com>
2022-06-25 21:38:32 +0530

    Added, indexing and improved readability

--- Data Structures/Dynamic Programming/KadanesAlgo.py
+++ Data Structures/Dynamic Programming/KadanesAlgo.py
@@ -2,11 +2,29 @@
 # Time: O(n), Space: O(1)
 def Kadane(arr):
     max_current = max_global = arr[0]
+    left_index = r_index = 0
     for i in range(1, len(arr)):
-        max_current = max(arr[i], max_current + arr[i])
+    # Fast solution
+
+    #     max_current = max(arr[i], max_current + arr[i])
+    #     if max_current > max_global:
+    #         max_global = max_current
+    # return max_global
+
+    # Returning Indexes
+        if arr[i] > max_current + arr[i]:
+            left_index = i
+            max_current = arr[i]
+        else:
+            max_current = arr[i] + max_current
+
         if max_current > max_global:
             max_global = max_current
-    return max_global
+            r_index = i 
 
-arr = [1,2,3,-2,5]
+    return left_index, r_index, arr[left_index: r_index], max_global
+
+
+
+arr = [-2, -3, 4, -1, -2, 1, 5, -3]
 print(Kadane(arr))
