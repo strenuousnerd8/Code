ebd268bc1d9991e37c2cabc377dfbb53518a13a9
strenuousnerd8 <zakkiwat@gmail.com>
2022-07-09 22:08:05 +0530

    solved!

---
+++ MaxOccurance.py
@@ -0,0 +1,36 @@
+eventToken = 'xyfidwg'
+
+def maxCharOccurance(word):
+    words = word.split()
+    my_dict = dict()
+    res = []
+    for i in words:
+        my_dict[i] = {}
+        for j in i:
+            if j not in my_dict[i]:
+                my_dict[i][j] = 1
+            else:
+                my_dict[i][j] += 1
+    for i in my_dict:
+        res.append(max(my_dict[i].values()))
+    return filterToken(words[findMax(res)])
+
+def findMax(arr):
+    count = globmax = 0
+    index = 0
+    for i in range(len(arr)):
+        if arr[i] > count:
+            count = arr[i]
+        if count > globmax:
+            globmax = count
+            index = i
+    return index
+
+def filterToken(word):
+    res = ""
+    for i in word:
+        if i not in eventToken:
+            res += i
+    return res
+
+print(maxCharOccurance(input()))
