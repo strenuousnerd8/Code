8b3f35a173dd9281386999a842532c17be82ad4d
strenuousnerd8 <zakkiwat@gmail.com>
2022-07-18 23:08:01 +0530

    3 added

--- TrianglePattern.py
+++ TrianglePattern.py
@@ -1,3 +1,16 @@
+# Triangle Pattern using Asterisk / Star
 n = int(input())
 for i in range(n, 0, -1):
     print(" " * i, ['* ' * i for i in range(1, n + 1)][::-1][i - 1])
+
+# Integers pattern with each line starting from 1
+n = int(input())
+for i in range(n, 0, -1):
+    print(" " * i, *[list(range(1, i + 1)) for i in range(1, n + 1)][::-1][i - 1], sep=" ")
+
+# Diamond Pattern using Asterisk / Star
+n = int(input())
+for i in range(n, 0, -1):
+    print(" " * i, ['* ' * i for i in range(1, n + 1)][::-1][i - 1])
+for i in range(1, n):
+    print(" " * (i + 1), ['* ' * i for i in range(1, n)][::-1][i - 1])
