bdeb9427b59cdc874d9871b92baae9fd04bf0f46
strenuousnerd8 <zakkiwat@gmail.com>
2022-07-21 22:26:29 +0530

    Solved!

---
+++ HackerRank/SuperReducedString.py
@@ -0,0 +1,30 @@
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def superReducedString(s):
+    # Write your code here
+    s = list(s)
+    i = 0
+    while i < len(s) - 1:
+        if s[i] == s[i + 1]:
+            del s[i]
+            del s[i]
+            i = 0
+            if len(s) == 0:
+                return 'Empty String'
+                break
+        else:
+            i+=1
+    return ''.join(s)
+
+if __name__ == '__main__':
+
+    s = input()
+
+    result = superReducedString(s)
+
+    print(result)
