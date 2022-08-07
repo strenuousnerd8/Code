c0cc624888f0e36c54edd1140119a5fdda05d76d
strenuousnerd8 <zakkiwat@gmail.com>
2022-08-07 17:45:25 +0530

    Solved in O(n)

---
+++ GoodnessOfAString.py
@@ -0,0 +1,25 @@
+import json
+from re import L
+def goodnessOfString(s):
+    # Write your code here.
+    dep = 0
+    res = 0
+    tes = ""
+    for i in s:
+        if i == '[':
+            dep += 1
+        elif i.isnumeric():
+            tes += i
+        elif i == ',':
+            if tes != "":
+                res += (int(tes) * dep)
+            tes = ""
+        elif i == ']':
+            if tes != "":
+                res += (int(tes) * dep)
+            tes = ""
+            dep -= 1
+    return res
+
+s = "[1,[2,3],[4,[5,6]]]"
+print(goodnessOfString(s))
