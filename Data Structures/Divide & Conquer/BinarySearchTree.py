c0fa2056d96ee84e5abd5319c19bea1929053137
strenuousnerd8 <zakkiwat@gmail.com>
2022-08-14 18:53:47 +0530

    Final revised!

---
+++ Data Structures/Divide & Conquer/BinarySearchTree.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.root is None:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+
+bn = BinarySearchTree()
+bn.insert(2)
+bn.insert(1)
+bn.insert(3)
+print(bn.root.value)
+print(bn.root.left.value)
+print(bn.root.right.value)
+
+print(bn.contains(2))
