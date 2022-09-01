10978891e79d760fc8ddeccbb7e4013121f5b67d
strenuousnerd8 <zakkiwat@gmail.com>
2022-09-01 15:08:22 +0530

    formatted

--- GeeksForGeeks/CountTheTriplets.py
+++ GeeksForGeeks/CountTheTriplets.py
@@ -1,29 +1,30 @@
-#User function Template for python3
+# User function Template for python3
 class Solution:
-	def countTriplet(self, arr, n):
-		# code here
-		count = 0
-		hashmap = {
-		    i : None for i in arr
-		}
-		for i in range(len(arr)):
-		    for j in range(i + 1, len(arr)):
-		        if arr[i] + arr[j] in hashmap:
-		            count += 1
-		return count
+    def countTriplet(self, arr, n):
+        # code here
+        count = 0
+        hashmap = {
+            i: None for i in arr
+        }
+        for i in range(len(arr)):
+            for j in range(i + 1, len(arr)):
+                if arr[i] + arr[j] in hashmap:
+                    count += 1
+        return count
 
-#{
+# {
  # Driver Code Starts
-#Initial Template for Python 3
+# Initial Template for Python 3
+
 
 if __name__ == '__main__':
-	T=int(input())
-	for i in range(T):
-		n = int(input())
-		arr = [int(x) for x in input().split()]
+    T = int(input())
+    for i in range(T):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
 
-		ob = Solution()
-		ans = ob.countTriplet(arr, n)
-		print(ans)
+        ob = Solution()
+        ans = ob.countTriplet(arr, n)
+        print(ans)
 
 # } Driver Code Ends
