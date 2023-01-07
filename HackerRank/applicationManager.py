335569e46a5947a33960b3b5ec4bca0e2042a3f6
FaizAhmed Akkiwat <zakkiwat@gmail.com>
2023-01-07 13:29:40 +0530

    new prog

---
+++ HackerRank/applicationManager.py
@@ -0,0 +1,22 @@
+def getOpenApplications(commands):
+    applications = []
+    for command in commands:
+        if command == "clear":
+            applications.clear()
+            continue
+        command, subject = command.split()
+        if command == "open":
+            applications.append(subject)
+        elif command == "close":
+            if int(subject) > len(applications):
+                applications.clear()
+                continue
+            for x in range(int(subject)):
+                applications.pop()
+    return applications
+                
+if __name__ == '__main__':
+    commands_count = 5
+    commands = ["clear", "open firefox", "close 4", "open firefox", "open curl", "close 1", "clear", "open ps"]
+    result = getOpenApplications(commands)
+    print(result)
